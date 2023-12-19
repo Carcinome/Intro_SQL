ff89cc323db63fa2c540a18818394a7fc820f699
Carcinome <154324411+Carcinome@users.noreply.github.com>
2023-12-19 11:15:40 +0100

    Add files via upload

---
+++ up-exemple-fil-rouge-data.sql
@@ -0,0 +1,80 @@
+INSERT INTO
+    Book(
+        title,
+        author_name,
+        nationality,
+        publication_year
+    )
+VALUES
+    (
+        'L’île au trésor',
+        'Stevenson',
+        'Royaume-Uni',
+        1883
+    ),
+    (
+        'Les Hauts de Hurle-Vent',
+        'Brontë',
+        'Royaume-Uni',
+        1847
+    ),
+    (
+        'Madame Bovary',
+        'Flaubert',
+        'France',
+        1869
+    ),
+    (
+        'Moby Dick',
+        'Melville',
+        'États-Unis',
+        1851
+    ),
+    (
+        'À la recherche du temps perdu',
+        'Proust',
+        'France',
+        1927
+    ),
+    (
+        'Demande à la poussière',
+        'Fante',
+        'États-Unis',
+        1939
+    ),
+    (
+        'Les raisins de la colère',
+        'Steinbeck',
+        'États-Unis',
+        1939
+    ),
+    (
+        'Don Quichotte',
+        'Cervantes',
+        'Espagnol',
+        1605
+    ),
+    (
+        'Cent ans de solitude',
+        'Marquez',
+        'Colombie',
+        1967
+    ),
+    (
+        'Hamlet',
+        'Shakespeare',
+        'Royaume-Uni',
+        1603
+    ),
+    (
+        'Bilbo le hobbit',
+        'Tolkien',
+        'Royaume-Uni',
+        1937
+    ),
+    (
+        'Les Robots',
+        'Asimov',
+        'États-Unis',
+        1950
+    );
